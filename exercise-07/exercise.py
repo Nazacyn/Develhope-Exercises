e9089c4971e69250bdf325937f9f830b6714b069
Nazacyn <114517797+Nazacyn@users.noreply.github.com>
2023-02-13 08:51:31 +0100

    Update exercise.py

--- exercise-07/exercise.py
+++ exercise-07/exercise.py
@@ -1,3 +1,9 @@
+# Functions - Exercise 7 🐍
+
+Create a lambda function that returns 2nd power of given number if its even   
+and run it on the given list   
+then print the result to the screen 
+
 my_list= [*range(5)] 
 
 # use anonymous function
